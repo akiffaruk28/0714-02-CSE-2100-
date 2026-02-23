fe714e580f62d546808ba9dc02e22909f52d1114
akiffaruk28 <akiffaruk58@gmail.com>
2026-02-24 02:32:49 +0600

    Delete New Version/src directory

--- New Version/src/progress_display.c
+++
@@ -1,8 +0,0 @@
-#include "../headers/progress_display.h"
-
-void update_status(const char *message, double progress) {
-    gtk_label_set_text(GTK_LABEL(status_label), message);
-    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_bar), progress);
-
-    while (gtk_events_pending()) gtk_main_iteration();
-}
